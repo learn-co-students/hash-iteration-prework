788d628224a836b3d9c22263d0e84331293de1a9
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-13 21:20:09 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,15 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
-# }
+#\ }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |room_assignment, passenger_name|
+    if passenger_name.start_with?('A') == true
+      return passengers[room_assignment]
+    end
+  end
 end
